f7096ddd5b1c34a67b78f0e84cd258271a9d811d
enzosison <59574038+enzosison@users.noreply.github.com>
2020-03-06 15:56:27 -0800

    Add files via upload

--- concordance.py
+++ concordance.py
@@ -6,11 +6,17 @@ class Concordance:
     def __init__(self):
         self.stop_table = None          # hash table for stop words
         self.concordance_table = None   # hash table for concordance
+        self.load_stop_table("stop_words.txt")
 
     def load_stop_table(self, filename):
         """ Read stop words from input file (filename) and insert each word as a key into the stop words hash table.
         Starting size of hash table should be 191: self.stop_table = HashTable(191)
         If file does not exist, raise FileNotFoundError"""
+        self.stop_table = HashTable(191)
+        f = open(filename)
+        for word in f:
+            word_split = word.rstrip()
+            self.stop_table.insert(word_split, 0)   #the insert function needs to take in a value so I just inserted 0 for each key
 
     def load_concordance_table(self, filename):
         """ Read words from input text file (filename) and insert them into the concordance hash table, 
@@ -21,7 +27,59 @@ class Concordance:
             If word is in table, get current value (list of line numbers), append new line number, insert (key, value)
             If word is not in table, insert (key, value), where value is a Python List with the line number
         If file does not exist, raise FileNotFoundError"""
+        self.concordance_table = HashTable(191)
+        linecounter = 0
+        try:
+            f = open(filename)
+        except:
+            raise FileNotFoundError
+        for line in f:
+            linecounter += 1
+            line = self.string_prep(line)
+            split_line = line.split()           #there's currently no check against stop words, punctuation, and numbers
+            for word in split_line:
+            #add another if statement here, that checks for bad inputs first
+                if self.concordance_table.get_value(word) and not self.stop_check(word):      #if the word already exists in our table
+                    new_value = self.concordance_table.get_value(word)
+                    if linecounter not in new_value:            #insert new line number as long as it's not a repeat
+                        new_value = new_value + [linecounter]
+                        self.concordance_table.insert(word, new_value)
+                else:                                           #if it's a whole new word
+                    self.concordance_table.insert(word, [linecounter])
+
+    def string_prep(self, line):
+        for i in line:
+            if i == "-":
+                x = line.replace("-"," ")
+            translator = str.maketrans('', '', string.punctuation)
+            return line.translate(translator).lower()
+
+    def check_num(self, word):
+        return str.replace('.','','1').isdigit()
+
+    def stop_check(self, word):
+        return not self.stop_table.in_table(word) and not self.check_num(word)
 
     def write_concordance(self, filename):
         """ Write the concordance entries to the output file(filename)
         See sample output files for format."""
+        word_list = []
+        d = open(filename, "w+")
+        for word in self.concordance_table.hash_table:
+            if word:
+                word_list.append(word)
+        sorted_list = sorted(word_list)
+        for index, entry in enumerate(sorted_list):
+            word = entry.key
+            value_list = entry.value     #value list is currently ints  -> convert to strings -> join strings
+            line_string = " ".join([str(i) for i in value_list])
+            d.write(word + ": " + line_string)
+            if index != len(sorted_list) - 1:
+                d.write("\n")
+
+
+
+
+
+
+
--- hash_quad.py
+++ hash_quad.py
@@ -1,3 +1,12 @@
+class Entry:
+    def __init__(self, k, v):
+        self.key = k
+        self.value = v
+    def __repr__(self):
+        return str(self.key, self.value)
+    def __lt__(self, other):
+        return self.key < other.key
+
 class HashTable:
 
     def __init__(self, table_size):         # can add additional attributes
@@ -13,31 +22,96 @@ class HashTable:
         If the key is is in the table, the new value replaces the existing value.
         When used with the concordance, value is a Python List of line numbers.
         If load factor is greater than 0.5 after an insertion, hash table size should be increased (doubled + 1)."""
+        entry = Entry(key, value)
+        # self.check_load()
+        index = self.horner_hash(entry.key)
+        j = 0
+        for i in range(0, self.table_size):
+            j = (index + i**2) % self.table_size  #quad probing in case of collision
+
+            if not self.hash_table[j]:   #insert if there is nothing there
+                self.hash_table[j] = entry
+                self.num_items += 1
+                self.check_load()
+                return
+
+            elif self.hash_table[j].key == entry.key:    #insert if it's the same word
+                self.hash_table[j] = entry
+                return
+
+    def grow_hash(self):
+        x = HashTable(self.table_size * 2 + 1)  #create new empty hash table
+        for i in self.hash_table:
+            if i:
+                x.insert(i.key, i.value)      #copy over values / previously x.insert(i)
+        self.hash_table = x.hash_table        #copy back to new hash table
+        self.table_size = x.table_size
+        self.num_items = x.num_items
+
+
+    def check_load(self):
+        load = self.num_items / self.table_size
+        if load > 0.5:
+            self.grow_hash()
 
     def horner_hash(self, key):
         """ Compute and return an integer from 0 to the (size of the hash table) - 1
         Compute the hash value by using Horner’s rule, as described in project specification."""
+        h = 0
+        for i in range(min(len(key), 8)):
+            h = h*31 + ord(key[i])
+        return h % self.table_size
 
     def in_table(self, key):
         """ Returns True if key is in an entry of the hash table, False otherwise. Must be O(1)."""
+        index = self.horner_hash(key)
+        j = 0
+        for i in range(0, self.table_size):
+            j = (index + i ** 2) % self.table_size
+            if self.hash_table[j] and self.hash_table[j].key == key:
+                return True
+        return False
 
     def get_index(self, key):
         """ Returns the index of the hash table entry containing the provided key. 
         If there is not an entry with the provided key, returns None. Must be O(1)."""
+        index = self.horner_hash(key)
+        j = 0
+        for i in range(0, self.table_size):
+            j = (index + i ** 2) % self.table_size
+            if self.hash_table[j] and self.hash_table[j].key == key:
+                return j
+        return None
 
     def get_all_keys(self):
         """ Returns a Python list of all keys in the hash table."""
+        listA = []
+        for entry in self.hash_table:
+            if entry != None:
+                listA.append(entry.key)
+        return listA
 
     def get_value(self, key):
         """ Returns the value (for concordance, list of line numbers) associated with the key.
         If key is not in hash table, returns None. Must be O(1)."""
+        index = self.horner_hash(key)
+        j = 0
+        for i in range(0, self.table_size):
+            j = (index + i ** 2) % self.table_size
+            if self.hash_table[j] and self.hash_table[j].key == key:
+                return self.hash_table[j].value
+        return None
 
     def get_num_items(self):
         """ Returns the number of entries (words) in the table. Must be O(1)."""
+        return self.num_items
 
     def get_table_size(self):
         """ Returns the size of the hash table."""
+        return self.table_size
 
     def get_load_factor(self):
         """ Returns the load factor of the hash table (entries / table_size)."""
+        load = self.num_items / self.table_size
+        return load
 
